14052f5d1f8d29d68e1c74bbdeb74fca7959356b
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-12-27 17:53:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,17 @@
 
 int main()
 {
-    const int lower = 0;
-    const int upper = 300;
+    float fahr, celsius;
+    float lower, upper, step;
+    lower = 0; /* lower limit of temperatuire scale */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
 
-    int fahr = lower;
+    fahr = lower;
     while (fahr <= upper) {
-        const int step = 20;
-        int celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
+
