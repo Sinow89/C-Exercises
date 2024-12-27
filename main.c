17a732ea875a216baf466b456a56d5efbc397008
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-12-27 18:02:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,9 +11,10 @@ int main()
     step = 20; /* step size */
 
     fahr = lower;
+    printf("Fahrenheit\tCelsius\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        printf("%10.0f\t%7.2f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
