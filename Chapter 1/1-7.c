2af9b64eda273a772a35cc802e9543617452e9da
Sinow89 <christoffer.rozenbachs@gmail.com>
2025-01-03 14:34:08 +0100

    Update 1-7.c

--- Chapter 1/1-7.c
+++ Chapter 1/1-7.c
@@ -3,19 +3,10 @@
 //
 
 #include <stdio.h>
-/* copy input to output; 2nd version */
+
 int main()
 {
-    int d = EOF;
-    printf("%d", d);
-    {
-        int c;
-        while ((c = getchar()) != EOF)
-            putchar(c);
-        return printf("%d", c);
+    int c = getchar() != EOF;
+    printf("%d", c);
 
-        if (EOF == 2) {
-            printf(0);
-        }
-    }
 }
