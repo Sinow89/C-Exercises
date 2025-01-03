e08bfb35dda5df28f930514fb8a0f883f8baf49d
Sinow89 <christoffer.rozenbachs@gmail.com>
2025-01-03 11:16:39 +0100

    Added next

---
+++ Chapter 1/1-6.c
@@ -0,0 +1,13 @@
+﻿//
+// Created by chris on 2025-01-03.
+//
+
+#include <stdio.h>
+/* copy input to output; 2nd version */
+int main()
+{
+    int c;
+    c = (getchar() != EOF);
+    printf("%d", c);
+
+}
---
+++ Chapter 1/1-7.c
@@ -0,0 +1,21 @@
+﻿//
+// Created by chris on 2025-01-03.
+//
+
+#include <stdio.h>
+/* copy input to output; 2nd version */
+int main()
+{
+    int d = EOF;
+    printf("%d", d);
+    {
+        int c;
+        while ((c = getchar()) != EOF)
+            putchar(c);
+        return printf("%d", c);
+
+        if (EOF == 2) {
+            printf(0);
+        }
+    }
+}
