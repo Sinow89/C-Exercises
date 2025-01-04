927a4829f199b125044a20b6ae4cb1794e75ee35
Sinow89 <christoffer.rozenbachs@gmail.com>
2025-01-04 14:58:03 +0100

    added 1-8.

---
+++ Chapter 1/1-8.c
@@ -0,0 +1,17 @@
+﻿//
+// Created by chris on 2025-01-04.
+//
+
+/* count characters in input; 1st version */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double nc;
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
+    system("pause");  // This keeps the window open
+    return 0;
+}
