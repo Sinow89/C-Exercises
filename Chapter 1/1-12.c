3ce86695a9948af24974d8dd2da0db7b91e5fffe
Sinow89 <christoffer.rozenbachs@gmail.com>
2025-01-04 16:39:49 +0100

    Create 1-12.c

---
+++ Chapter 1/1-12.c
@@ -0,0 +1,27 @@
+﻿//
+// Created by chris on 2025-01-04.
+//
+/* count lines, words, and characters in input */
+#include <stdio.h>
+
+int main() {
+    int c, in_word = 0;
+
+    // Read input until EOF
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            // If the character is a space, tab, or newline, mark that we are outside a word
+            if (in_word) {
+                putchar('\n');  // Print a newline after a word
+                in_word = 0;    // Reset word status
+            }
+        } else {
+            // If the character is not a space, tab, or newline, we are inside a word
+            if (!in_word) {
+                in_word = 1;   // Mark that we are inside a word
+            }
+            putchar(c);       // Print the character of the word
+        }
+    }
+    return 0;
+}
