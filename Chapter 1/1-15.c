c7bc229a067a3711d2845850da96ea2b2fb327f1
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-01-11 21:28:48 +0100

    Update 1-15.c

--- Chapter 1/1-15.c
+++ Chapter 1/1-15.c
@@ -3,24 +3,19 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
 
 /* test power function */
-int power(int m, int n);
+int fahr();
 
 int main()
 {
-    int i;
-    for (i = 0; i < 10; ++i)
-        printf("%d %d %d\n", i, power(2,i), power(-3,i));
+    fahr();
     return 0;
 }
 
-int power(int base, int n)
+int fahr()
 {
-    int i, p;
-    p = 1;
-    for (i = 1; i <= n; ++i)
-        p = p * base;
-    return p;
+    int fahr;
+    for (fahr = 0; fahr <= 300; fahr = fahr+20)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
